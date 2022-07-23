536a907a037c52f1f3264a96038f2fac6a1849e5
Prophet-C <samiya578fhj@gmail.com>
2022-07-23 11:21:51 +0800

    remove some useless code

--- networks/dinknet.py
+++ networks/dinknet.py
@@ -82,61 +82,7 @@ class DecoderBlock(nn.Module):
         x = self.norm3(x)
         x = self.relu3(x)
         return x
-    
-class DinkNet34_less_pool(nn.Module):
-    def __init__(self, num_classes=1):
-        super(DinkNet34_more_dilate, self).__init__()
-
-        filters = [64, 128, 256, 512]
-        resnet = models.resnet34(pretrained=True)
-        
-        self.firstconv = resnet.conv1
-        self.firstbn = resnet.bn1
-        self.firstrelu = resnet.relu
-        self.firstmaxpool = resnet.maxpool
-        self.encoder1 = resnet.layer1
-        self.encoder2 = resnet.layer2
-        self.encoder3 = resnet.layer3
-        
-        self.dblock = Dblock_more_dilate(256)
-
-        self.decoder3 = DecoderBlock(filters[2], filters[1])
-        self.decoder2 = DecoderBlock(filters[1], filters[0])
-        self.decoder1 = DecoderBlock(filters[0], filters[0])
-
-        self.finaldeconv1 = nn.ConvTranspose2d(filters[0], 32, 4, 2, 1)
-        self.finalrelu1 = nonlinearity
-        self.finalconv2 = nn.Conv2d(32, 32, 3, padding=1)
-        self.finalrelu2 = nonlinearity
-        self.finalconv3 = nn.Conv2d(32, num_classes, 3, padding=1)
-
-    def forward(self, x):
-        # Encoder
-        x = self.firstconv(x)
-        x = self.firstbn(x)
-        x = self.firstrelu(x)
-        x = self.firstmaxpool(x)
-        e1 = self.encoder1(x)
-        e2 = self.encoder2(e1)
-        e3 = self.encoder3(e2)
-        
-        #Center
-        e3 = self.dblock(e3)
-
-        # Decoder
-        d3 = self.decoder3(e3) + e2
-        d2 = self.decoder2(d3) + e1
-        d1 = self.decoder1(d2)
-
-        # Final Classification
-        out = self.finaldeconv1(d1)
-        out = self.finalrelu1(out)
-        out = self.finalconv2(out)
-        out = self.finalrelu2(out)
-        out = self.finalconv3(out)
-
-        return F.sigmoid(out)
-    
+       
 class DinkNet34(nn.Module):
     def __init__(self, num_classes=1, num_channels=3):
         super(DinkNet34, self).__init__()
